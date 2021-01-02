61b25315b8cd50ef372e55b4e07b9940f8e81bae
serena-chen <sacdesign1@gmail.com>
2021-01-02 13:57:47 -0500

    overflow scroll test 2

--- v2/assets/js/main.js
+++ v2/assets/js/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
+  function resetHeight(){
+      // reset the body height to that of the inner browser
+      document.body.style.height = window.innerHeight + "px";
+  }
+  // reset the height whenever the window's resized
+  window.addEventListener("resize", resetHeight);
+  // called to initially set the height.
+  resetHeight();
+
 
 //// SIDE NAV TITLE ON PROJECT PAGE SELECT
 
